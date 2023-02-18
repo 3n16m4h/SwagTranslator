948f3a63f8442bdd760326703b6e311a641f4d7f
Vinny <121131442+VicieVaiPiano@users.noreply.github.com>
2023-02-18 17:12:59 +0100

    Swaggy Update 2.0
    Interpolation escape of the quotation marks.
    Used black formatting

--- swag.py
+++ swag.py
@@ -12,33 +12,34 @@ print(colored(swag_ascii, swag_color))
 # Input
 message = input("Inserisci la stringa da tradurre: ")
 
+
 # C++
-cpp_translation = 'std::cout << "' + message + '" << std::endl;'
+cpp_translation = 'std::cout << "' + message.replace('"', '\\"') + '" << std::endl;'
 cpp_color = random.choice(["red", "green", "yellow", "blue", "magenta", "cyan"])
 print("C++:", colored(cpp_translation, cpp_color))
 
 # C#
-csharp_translation = 'Console.WriteLine("' + message + '");'
+csharp_translation = 'Console.WriteLine("' + message.replace('"', '\\"') + '");'
 csharp_color = random.choice(["red", "green", "yellow", "blue", "magenta", "cyan"])
 print("C#:", colored(csharp_translation, csharp_color))
 
 # Java
-java_translation = 'System.out.println("' + message + '");'
+java_translation = 'System.out.println("' + message.replace('"', '\\"') + '");'
 java_color = random.choice(["red", "green", "yellow", "blue", "magenta", "cyan"])
 print("Java:", colored(java_translation, java_color))
 
 # JavaScript
-js_translation = 'console.log("' + message + '");'
+js_translation = 'console.log("' + message.replace('"', '\\"') + '");'
 js_color = random.choice(["red", "green", "yellow", "blue", "magenta", "cyan"])
 print("JavaScript:", colored(js_translation, js_color))
 
 # PHP
-php_translation = 'echo "' + message + '";'
+php_translation = 'echo "' + message.replace('"', '\\"') + '";'
 php_color = random.choice(["red", "green", "yellow", "blue", "magenta", "cyan"])
 print("PHP:", colored(php_translation, php_color))
 
 # HTML
-html_translation = "<p>" + message + "</p>"
+html_translation = "<p>" + message.replace('"', '\\"') + "</p>"
 html_color = random.choice(["red", "green", "yellow", "blue", "magenta", "cyan"])
 print("HTML:", colored(html_translation, html_color))
 
@@ -48,27 +49,27 @@ bf_color = random.choice(["red", "green", "yellow", "blue", "magenta", "cyan"])
 print("Brainfuck:", colored(bf_translation, bf_color))
 
 # Rust
-rust_translation = 'println!("' + message + '");'
+rust_translation = 'println!(\"' + message + '\");'
 rust_color = random.choice(["red", "green", "yellow", "blue", "magenta", "cyan"])
 print("Rust:", colored(rust_translation, rust_color))
 
 # R
-r_translation = 'print("' + message + '")'
+r_translation = 'print(\"' + message + '\")'
 r_color = random.choice(["red", "green", "yellow", "blue", "magenta", "cyan"])
 print("R:", colored(r_translation, r_color))
 
 # Swift
-swift_translation = 'print("' + message + '")'
+swift_translation = 'print(\"' + message + '\")'
 swift_color = random.choice(["red", "green", "yellow", "blue", "magenta", "cyan"])
 print("Swift:", colored(swift_translation, swift_color))
 
 # Go
-go_translation = 'fmt.Println("' + message + '")'
+go_translation = 'fmt.Println(\"' + message + '\")'
 go_color = random.choice(["red", "green", "yellow", "blue", "magenta", "cyan"])
 print("Go:", colored(go_translation, go_color))
 
 # Ruby
-ruby_translation = 'puts "' + message + '"'
+ruby_translation = 'puts \"' + message + '\"'
 ruby_color = random.choice(["red", "green", "yellow", "blue", "magenta", "cyan"])
 print("Ruby:", colored(ruby_translation, ruby_color))
 
